1e2368d64ef20ef627634c35c4590b792bfcf60c
alecpietersz <80108884+alecpietersz@users.noreply.github.com>
2022-12-08 17:33:21 +0100

    implemented variables and constraints

--- SimpleVRP.py
+++ SimpleVRP.py
@@ -2,38 +2,60 @@ from gurobipy import Model, quicksum, GRB
 from numpy import *
 from openpyxl import *
 from time import *
+from generate_input import generateInput
 
-class CityPair:
-    def __init__(self, origin, destination, cost):
-        self.From   = origin
-        self.To     = destination
-        self.Cost   = cost
-
-class City:
-    def __init__(self, name):
-        self.Name   = name
-
-def VRP_Problem (CityPairs):
+def VRP_Problem (depots, customers, trucks, nodes):
     
     model = Model("VRP")                # LP model (this is an object)
     
-    x = {}                              # Decision Variables (DVs)
-    for ij in range(len(CityPairs)):
-        if CityPairs[ij].From != CityPairs[ij].To:
-            x[CityPairs[ij].From,CityPairs[ij].To] = model.addVar(obj=CityPairs[ij].Cost, vtype ="B",
-                                                name = "x"+CityPairs[ij].From+CityPairs[ij].To)
+    x = {} # BV indicating the use of the path between nodes i,j
+    for node_i in nodes:
+        for node_j in nodes:
+            x[node_i.id,node_j.id] = model.addVar(vtype ="B", name = f"x_{node_i.id}{node_j.id}")
+    
+    y = {} # BV indicating the use of facility i
+    for depo in depots:
+        y[depo.id] = model.addVar(vtype = "B", name = f"y_{depo.id}")
+
+    f = {} # BV indicating if customer at node j is served by route that starts at depo at node i
+    for node_i in depots:
+        for node_j in customers:
+            f[node_i.id,node_j.id] = model.addVar(vtype = "B", name =  f"x_{node_i.id}{node_j.id}")
+    
+    z = {} # BV if customer at node j is last one served in route
+    for node_j in customers:
+        z[node_j.id] = model.addVar(vtype = "B", name = f"z_{node_j.id}")
+
+    a = {} # BV that indicates if a vehicle uses path j to return from the end of its route (at node j) to a depot (at node i)
+    for node_i in depots:
+        for node_j in customers:
+            a[node_i.id,node_j.id] = model.addVar(vtype = "B", name = f"a_{node_i.id}{node_j.id}")
+
+    t = {} # CV indicating the amount of cargo transported between nodes i and j
+    for node_i in depots:
+        for node_j in customers:
+            t[node_i.id,node_j.id] = model.addVar(vtype = "B", name = f"t_{node_i.id}{node_j.id}")
     
     model.update()                      
 
-    Entering = {}                       # build 'capacity' constraints
-    for i in range(len(Cities)):
-        Entering[Cities[i].Name] = model.addConstr(quicksum(x[Cities[i].Name,Cities[j].Name] for j in range(len(Cities))),
-                                                             '=', 1, name = 'Entering'+Cities[i].Name)                     
-    
-    Leaving = {}                       # build 'capacity' constraints
-    for i in range(len(Cities)):
-        Leaving[Cities[i].Name] = model.addConstr(quicksum(x[Cities[j].Name,Cities[i].Name] for j in range(len(Cities))),
-                                                             '=', 1, name = 'Leaving'+Cities[i].Name)   
+    customer_visited = {} # the number of arcs arriving to a customer node must be 1, that is, every customer node is visited by a route.
+    for node_j in customers:
+        customer_visited[node_j.id] = model.addConstr(quicksum(x[node_i.id,node_j.id] for node_i in nodes),
+                                                     '=', 1, name = f'customer_visited{node_j.id}')
+
+    flow_conservation = {} #the sum of the arcs of output of a demand is equal to the sum of the input arcs:
+    for node_j in customers:
+        flow_conservation[node_j.id] = model.addConstr(quicksum(x[node_j.id,node_k.id] for node_k in customers)
+                                                    +  quicksum(a[node_i.id,node_j.id] for node_i in depots),
+                                                    '=', quicksum(x[node_i.id,node_j.id] for node_i in nodes),
+                                                    name = f"flow_cons{node_j.id}") 
+
+    return_to_depot = {} # for a facility i, the number of output arcs x must be equal to the number of arcs of arrival a                                                                              
+    for node_i in depots:
+        return_to_depot[node_i.id] = model.addConstr(quicksum(x[node_i.id,node_j.id] for node_j in customers),
+                                                '=', quicksum(a[node_i.id,node_j.id] for node_j in customers),
+                                                name = f'return_to_depot{node_i.id}')
+        
 
     model.update()
     model.write("VRP_Model.lp")
@@ -56,13 +78,7 @@ def VRP_Problem (CityPairs):
 
 
     print
-    # for k in range(len(Arcs)):
-    #     Flow = 0
-    #     for m in range(len(Commodities)):
-    #         Flow += x[m,Arcs[k].From,Arcs[k].To].X
-    #     if int(Flow)>0:
-    #         print ("Arc(%d,%d) \t" %(Arcs[k].From + 1,Arcs[k].To + 1), int(Flow))
-    #         print
+   
     print
     print ("Objective Function =", model.ObjVal/1.0)
     print ("------------------------------------------------------------------------")
@@ -73,30 +89,20 @@ def VRP_Problem (CityPairs):
 if __name__ == '__main__':
     #=================================================================================================
     # Input excel file with arcs data (sheet1) and commodities data (sheet2)
-    CityPairs       = []
-    Cities          = []
-    
-    wb = load_workbook("SimpleVRPData.xlsx", read_only=True)
-    List_city_pairs = tuple(wb["Sheet1"].iter_rows())
 
-    for i, origin in enumerate(List_city_pairs[1:]):
-        for j, destination in enumerate(origin[1:]):
-            new_pair = CityPair(List_city_pairs[i+1][0].value,List_city_pairs[0][j+1].value,int(destination.value))
-            CityPairs.append(new_pair)
+    # I, J
+    depots, customers, trucks = generateInput(3,3,1)
 
-    for i, city in enumerate(List_city_pairs[0][1:]):
-        new = City(city.value)
-        Cities.append(new)
+    # V
+    nodes = [*depots, *customers]    
 
-    for pair in Cities:
-        print(vars(pair))
+    for object in nodes:
+        print(vars(object))
 
-    
-    # del new, NewNode
     #=================================================================================================
     start_time = time()
-    # RUN MCF PROBLEM
-    VRP_Problem(CityPairs)
+    
+    VRP_Problem(depots, customers, trucks, nodes)
     
     elapsed_time = time() - start_time
 
